90a3b0d78b384ed8d42c5ce25e0874b5bf01a97e
jhuliocastro <jhuliocastro@hotmail.com>
2022-02-08 23:01:32 -0300

    IMPLEMENTACAO DE GPIO DIRETO NO PHP

--- src/controller/Comandos.php
+++ src/controller/Comandos.php
@@ -3,19 +3,23 @@ namespace Controllers;
 
 use Controllers\Status;
 use Alertas\Alert;
-use Controllers\Python;
+use PiPHP\GPIO\GPIO;
+use PiPHP\GPIO\Pin\PinInterface;
 
 class Comandos{
     public function luzQuarto(){
-        $python = new Python();
         $status = new Status();
         $retorno = $status->statusLuzQuarto1();
         if($retorno == "0"){
-            $python->luzQuartoDesliga();
+            $gpio = new GPIO();
+            $pin = $gpio->getOutputPin(3);
+            $pin->setValue("1");
             $this->gravarArquivo("/var/www/html/db/statusLuzQuarto.yali", "1");
             Alert::success("Luz Desligada!", "Quarto", "/");
         }else if($retorno == "1"){
-            $python->luzQuartoLiga();
+            $gpio = new GPIO();
+            $pin = $gpio->getOutputPin(3);
+            $pin->setValue("0");
             $this->gravarArquivo("/var/www/html/db/statusLuzQuarto.yali", "0");
             Alert::success("Luz Ligada!", "Quarto", "/");
         }
